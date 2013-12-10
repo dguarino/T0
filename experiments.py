dc201ec89e24442ac958208049274b2f77939ef5
Domenico Guarino <domenico.guarino@gmail.com>
2013-12-10 14:58:56 +0100

    Working set of parameter from SceniakChatterjeeCallaway2006 fro Spatial filter and from AllenFreeman2006 for temporal filter

--- experiments.py
+++ experiments.py
@@ -6,20 +6,6 @@ from parameters import ParameterSet
 
 def create_experiments(model):
     return [
-    
-            # EXTRA-CLASSICAL SIZE TUNING (GRATING DISCS)
-            MeasureSizeTuning(
-                model, 
-                num_sizes=4, 
-                max_size=16.0, 
-                orientation=numpy.pi/2, 
-                spatial_frequency=0.8, 
-                temporal_frequency=2,
-                grating_duration=147*7,
-                contrasts=[100],
-                num_trials=4,
-                log_spacing=True
-            ),
             
             # CONTRAST CONTROL
             #MeasureContrastSensitivity(
@@ -32,6 +18,20 @@ def create_experiments(model):
             #    contrasts=[0,10,50,80,100],#[0,10,20,30,40,50,60,70,80,90,100],
             #    num_trials=4
             #),
+    
+            # EXTRA-CLASSICAL SIZE TUNING (GRATING DISCS)
+            MeasureSizeTuning(
+                model, 
+                num_sizes=14, 
+                max_size=16.0, 
+                orientation=numpy.pi/2, 
+                spatial_frequency=0.8, 
+                temporal_frequency=2,
+                grating_duration=147*7,
+                contrasts=[100],
+                num_trials=4,
+                log_spacing=True
+            ),
 
             # MASKING
             
